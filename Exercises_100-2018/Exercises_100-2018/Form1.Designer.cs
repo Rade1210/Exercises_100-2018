dbace7da280bd16b566d9e9f869d2722b14ec54e
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2020-11-10 20:21:56 +0100

    Promenjeno ime listbox0u i napravljena tabela

--- Exercises_100-2018/Exercises_100-2018/Form1.Designer.cs
+++ Exercises_100-2018/Exercises_100-2018/Form1.Designer.cs
@@ -28,23 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listboxPrintData = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // listboxPrintData
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 48);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(432, 212);
-            this.listBox1.TabIndex = 0;
+            this.listboxPrintData.FormattingEnabled = true;
+            this.listboxPrintData.Location = new System.Drawing.Point(31, 48);
+            this.listboxPrintData.Name = "listboxPrintData";
+            this.listboxPrintData.Size = new System.Drawing.Size(432, 212);
+            this.listboxPrintData.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(509, 359);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listboxPrintData);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +53,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listboxPrintData;
     }
 }
 
